5cfc40bc019523f261e6fada8d3d1bc75bc1c69a
MartinLetocha <martin.letocha2@gmail.com>
2022-11-09 12:17:00 +0100

    LookAtCameraScript added, wooo

---
+++ Assets/Scripts/LookAtCameraScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCameraScript : MonoBehaviour
+{
+    public Transform camera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation(camera.position - transform.position, Vector3.up);
+        transform.eulerAngles = new Vector3(camera.eulerAngles.x, rotation.eulerAngles.y - 180, rotation.eulerAngles.z);
+        Debug.Log(rotation + " " + transform.rotation);
+    }
+}
